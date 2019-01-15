6b7b87360751c4e40301659edef8da65205af0ed
Sean Watkins <seanlw@gmail.com>
2019-01-15 10:53:00 -0600

    Add Image work

---
+++ app/forms/hyrax/image_form.rb
@@ -0,0 +1,12 @@
+# Generated via
+#  `rails generate hyrax:work Image`
+module Hyrax
+  # Generated form for Image
+  class ImageForm < Hyrax::Forms::WorkForm
+
+    include Bcdams::AllFormsSharedBehaviour
+
+    self.model_class = ::Image
+    
+  end
+end
