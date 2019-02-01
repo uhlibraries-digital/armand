6bce7bc787acccad056cec4589e06489a1d81c6a
Sean Watkins <slwatkins@uh.edu>
2019-02-01 11:40:32 -0600

    Fix title being singular

--- app/forms/hyrax/text_form.rb
+++ app/forms/hyrax/text_form.rb
@@ -6,5 +6,24 @@ module Hyrax
     include Bcdams::AllFormsSharedBehaviour
 
     self.model_class = ::Text
+
+    def self.model_class?(field)
+      if [:title].include? field.to_sym
+        false
+      else
+        super
+      end
+    end
+
+    def self.model_attributes(_)
+      attrs = super
+      attrs[:title] = Array(attrs[:title]) if attrs[:title]
+      attrs
+    end
+
+    def title
+      super.first || ""
+    end
+
   end
 end
