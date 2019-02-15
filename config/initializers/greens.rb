1cffc419ff682c021cf2760c266373951fc658ab
Sean Watkins <slwatkins@uh.edu>
2019-02-15 14:08:28 -0600

    Minor fix

--- config/initializers/greens.rb
+++ config/initializers/greens.rb
@@ -1,3 +1,3 @@
-GreensClient::Ark.base_url = Settings.greens.endpoint
-GreensClient::Ark.prefix = Settings.greens.prefix
-GreensClient::Ark.api_key = Settings.greens.api_key
+GreensClient::Ark.base_url = Settings.greens.endpoint ||= nil
+GreensClient::Ark.prefix = Settings.greens.prefix ||= nil
+GreensClient::Ark.api_key = Settings.greens.api_key ||= nil
