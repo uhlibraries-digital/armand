0b421ecacc257657136b660c0088ebd7f48d13e3
Sean Watkins <slwatkins@uh.edu>
2019-03-14 14:36:47 -0500

    Add registered user authentication via IP range

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,12 @@ class ApplicationController < ActionController::Base
 
   before_action :require_active_account!
 
+  def current_ability
+    session_opts ||= user_session
+    session_opts ||= {}
+    @current_ability ||= Ability.new(current_user, session_opts.merge(remote_ip: request.remote_ip))
+  end
+
   private
 
     def require_active_account!
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,18 +4,27 @@ class Ability
   include Hyrax::Ability
   #self.ability_logic += [:everyone_can_create_curation_concerns]
 
+  def user_groups
+    return @user_groups if @user_groups
+
+    @user_groups = default_user_groups
+    @user_groups |= current_user.groups if current_user and current_user.respond_to? :groups
+    @user_groups |= ['registered'] unless current_user.new_record?
+    @user_groups |= [@options[:remote_ip]] if @options.present? and @options.has_key? :remote_ip
+    @user_groups
+  end
+
   # Define any customized permissions here.
   def custom_permissions
-    # Limits deleting objects to a the admin user
-    #
-    # if current_user.admin?
-    #   can [:destroy], ActiveFedora::Base
-    # end
+    
+    if !Settings.campus_ip.nil?
+      ips = Settings.campus_ip.split(',').map(&:strip).collect do |ip|
+        addr = IPAddr.new(ip) rescue next
+        addr.to_range.map(&:to_s)
+      end
+      ip_addrs = ips.flatten.compact.uniq || []
+      @user_groups |= ['registered'] if @options.present? and @options.has_key? :remote_ip and ip_addrs.include? @options[:remote_ip]
+    end
 
-    # Limits creating new objects to a specific group
-    #
-    # if user_groups.include? 'special_group'
-    #   can [:create], ActiveFedora::Base
-    # end
   end
 end
