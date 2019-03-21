c8b57a20ca65ef0af1f11b94fde6e634ab146ce6
Sean Watkins <slwatkins@uh.edu>
2019-03-21 10:59:32 -0500

    Add copy/paste keyboard shortcut to permalink

--- app/assets/javascripts/permalink.js
+++ app/assets/javascripts/permalink.js
@@ -1,10 +1,23 @@
 Blacklight.onLoad(function () {
+  var ctrlDown = false
+
   $('#permalink-copy-btn').on('click', function (e) {
     $('#permalink-text').select();
     document.execCommand("copy");
   })
   $('#permalink-text').on('keydown', function (e) {
+    if (e.keyCode == 17 || e.keyCode == 91) {
+      ctrlDown = true
+      return;
+    }
+    if (e.keyCode == 67 && ctrlDown) {
+      return;
+    }
     e.preventDefault();
     e.stopPropagation();
+  }).on('keyup', function (e) {
+    if (e.keyCode == 17 || e.keyCode == 91) {
+      ctrlDown = false
+    }
   })
 })
