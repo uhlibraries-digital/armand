91719755f55b9e0bf5b945a9a9b0f1307af30d3e
Sean Watkins <slwatkins@uh.edu>
2019-03-22 12:55:06 -0500

    Fix Rights Statement not saving

--- app/services/bcdams_metadata_service.rb
+++ app/services/bcdams_metadata_service.rb
@@ -1,7 +1,10 @@
 class BcdamsMetadataService < HydraEditor::FieldMetadataService
   def self.multiple?(model_class, field)
-    if [:title].include? field.to_sym
+    case field.to_s
+    when 'rights_statement', 'title'
       false
+    when 'ordered_member_ids', 'in_works_ids', 'member_of_collection_ids'
+      true
     else
       super
     end
