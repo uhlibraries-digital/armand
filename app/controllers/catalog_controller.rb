9929d91e74aa3e4a27d7e56774446a1d31c10c46
Sean Watkins <slwatkins@uh.edu>
2019-04-29 11:19:14 -0500

    Remove masonry and slideshow views in search results

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -15,8 +15,8 @@ class CatalogController < ApplicationController
 
   configure_blacklight do |config|
     config.view.gallery.partials = [:index_header, :index]
-    config.view.masonry.partials = [:index]
-    config.view.slideshow.partials = [:index]
+    # config.view.masonry.partials = [:index]
+    # config.view.slideshow.partials = [:index]
 
 
     config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
@@ -25,7 +25,7 @@ class CatalogController < ApplicationController
 
     # Show gallery view
     config.view.gallery.partials = [:index_header, :index]
-    config.view.slideshow.partials = [:index]
+    # config.view.slideshow.partials = [:index]
 
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
     config.default_solr_params = {
