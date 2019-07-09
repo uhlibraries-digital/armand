7ca4c6856ff81e4451998348419a9d04c90ce04b
Sean Watkins <slwatkins@uh.edu>
2019-07-09 09:12:52 -0500

    Show/Hide download options based on selected options

--- app/presenters/bcdams/shared_presenter.rb
+++ app/presenters/bcdams/shared_presenter.rb
@@ -7,12 +7,24 @@ module Bcdams
       delegate :alternative, :date, :extent, :format, :genre,
                :place, :series_title, :time_period, :resource_access_rights,
                :aspaceurl, :donor, :note, :related_item, :preservation_location, 
-               :digital_object_ark, :rights_holder, :provenance,
+               :digital_object_ark, :rights_holder, :provenance, :download_option,
                to: :solr_document
     
       def permalink
         return Settings.greens.base_url + digital_object_ark.first unless digital_object_ark.first.nil? || digital_object_ark.first.blank?
         request.original_url
+      end
+
+      def download_all?
+        download_option.first == Armand::DownloadControl::DOWNLOAD_TEXT_VALUE_ALL || download_option.first.nil?
+      end
+
+      def download_low?
+        download_option.first == Armand::DownloadControl::DOWNLOAD_TEXT_VALUE_LOW
+      end
+
+      def download_none?
+        download_option.first == Armand::DownloadControl::DOWNLOAD_TEXT_VALUE_NONE
       end
 
       def humanize_date
