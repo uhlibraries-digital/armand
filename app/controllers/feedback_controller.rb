8cb7087d9630fc6cad5d91b665daccd6d2ae4ea9
Sean Watkins <slwatkins@uh.edu>
2019-07-15 14:30:02 -0500

    Add recaptcha

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,12 +1,18 @@
 class FeedbackController < ApplicationController
 
   def generate
-    FeedbackMailer.feedback_email(
-      from: ActionController::Base.helpers.strip_tags(params[:email].first),
-      user_agent: request.user_agent, 
-      url: ActionController::Base.helpers.strip_tags(params[:object_url]),
-      type: ActionController::Base.helpers.strip_tags(params[:feedback_type]), 
-      comment: ActionController::Base.helpers.strip_tags(params[:comments])
-    ).deliver_now
+
+    if verify_recaptcha
+      FeedbackMailer.feedback_email(
+        from: ActionController::Base.helpers.strip_tags(params[:email].first),
+        user_agent: request.user_agent, 
+        url: ActionController::Base.helpers.strip_tags(params[:object_url]),
+        type: ActionController::Base.helpers.strip_tags(params[:feedback_type]), 
+        comment: ActionController::Base.helpers.strip_tags(params[:comments])
+      ).deliver_now
+    else
+      raise "Invalid reCAPTCHA check, you might be a robot"
+    end
+
   end
 end
