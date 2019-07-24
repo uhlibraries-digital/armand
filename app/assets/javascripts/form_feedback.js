475d47119890f3592ee8633f38eabacc5e9501aa
Sean Watkins <slwatkins@uh.edu>
2019-07-24 09:36:57 -0500

    Add "Thank You" and error response in feedback from

--- app/assets/javascripts/form_feedback.js
+++ app/assets/javascripts/form_feedback.js
@@ -4,19 +4,36 @@ $(document).ready(function () {
   })
 
   $('#feedback-form').submit(function (e) {
+    e.preventDefault();
     if ($("input[name=feedback_type]").val() === '') {
       alert('Please select the type of feedback');
-      e.preventDefault();
       return;
     }
 
     if ($('#comments').val() === '') {
       alert('Please add a comment');
-      e.preventDefault();
       return;
     }
 
-    $('#feedback-save-btn').text('Thank you')
-    $('#feedback-save-btn').attr("disabled", true)
+    $('#feedback-save-btn').attr("disabled", true);
+    $('#feedback-save-btn').addClass("sending");
+    $('#feedback-save-btn').text('Sending');
+    $('#feedback-container .error').hide();
+
+    $.ajax({
+      type: "POST",
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      success: function (data) {
+        $('#feedback-container .error').hide();
+        $('#feedback-container').html('<div class="feedback-thankyou">Thank you for your feedback</div>');
+      },
+      error: function (err) {
+        $('#feedback-container .error').show();
+        $('#feedback-save-btn').attr("disabled", false);
+        $('#feedback-save-btn').removeClass("sending");
+        $('#feedback-save-btn').text('Send');
+      }
+    });
   })
 })
