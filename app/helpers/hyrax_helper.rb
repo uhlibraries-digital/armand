351e2839310b6eb687ac291aa522d90100780a30
Sean Watkins <slwatkins@uh.edu>
2019-09-19 11:03:34 -0500

    Handle Blacklight exception with collection title by id

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -3,4 +3,16 @@ module HyraxHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
   include DownloadHelper
+
+  def collection_title_by_id(id)
+    begin
+      solr_docs = controller.repository.find(id).docs
+      return nil if solr_docs.empty?
+    rescue Blacklight::Exceptions::RecordNotFound
+      return nil
+    end
+    solr_field = solr_docs.first[ActiveFedora.index_field_mapper.solr_name("title", :stored_searchable)]
+    return nil if solr_field.nil?
+    solr_field.first
+  end
 end
